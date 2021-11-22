c628d8690cbab6f2a2a2fb183cab27735a90be4c
jflondonog <jflondonog@eafit.edu.co>
2021-11-22 15:11:27 -0500

    Deploy Front-End

---
+++ server.js
@@ -0,0 +1,15 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/PQR-System'));
+
+app.get('/*', (req, res) =>
+    res.sendFile(__dirname +  '/dist/PQR-System/index.html'),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
